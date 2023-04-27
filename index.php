3a04c9af6be69f25914e2da32f37823ed84c2d9c
sptndngrh <septiandin92@gmail.com>
2023-04-27 22:59:45 +0700

    First commit

---
+++ index.php
@@ -0,0 +1,124 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous" />
+    <link rel="stylesheet" href="./assets/css/style.css" />
+    <title>Katalog Kami</title>
+  </head>
+  <body>
+    <nav class="navbar navbar-expand-lg navbar-dark" style="background-color: #333c4a">
+      <a class="navbar-brand" href="#">Loobi</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+        <div class="navbar-nav">
+          <a class="nav-link" href="#">Produk</a>
+          <a class="nav-link" href="#">Tentang Kami</a>
+        </div>
+      </div>
+    </nav>
+
+    <div class="jumbotron">
+      <h1 class="display-4">Selamat Datang!</h1>
+      <p class="lead">Temukan pakaian yang cocok dengan kepribadian anda, dengan kualitas baik dan harga terjangkau</p>
+      <hr class="my-4" />
+      <div class="card-deck">
+        <div class="card">
+          <img src="./assets/picture/produka.jpg" class="card-img-top" alt="Produk a" />
+          <div class="card-body">
+            <h5 class="card-title">Kaos A</h5>
+            <p class="card-text">Rp 96.000</p>
+            <p class="card-text"><small class="text-muted">Stok 123</small></p>
+            <a class="btn btn-primary btn-lg" href="#" role="button">Beli Sekarang</a>
+          </div>
+        </div>
+        <div class="card">
+          <img src="./assets/picture/produkb.jpg" class="card-img-top" alt="Produk b" />
+          <div class="card-body">
+            <h5 class="card-title">Kaos B</h5>
+            <p class="card-text">Rp 150.000</p>
+            <p class="card-text"><small class="text-muted">Stok 226</small></p>
+            <a class="btn btn-primary btn-lg" href="#" role="button">Beli Sekarang</a>
+          </div>
+        </div>
+        <div class="card">
+          <img src="./assets/picture/produkc.jpg" class="card-img-top" alt="Produk c" />
+          <div class="card-body">
+            <h5 class="card-title">Kaos C</h5>
+            <p class="card-text">Rp 230.000</p>
+            <p class="card-text"><small class="text-muted">Stok 90</small></p>
+            <a class="btn btn-primary btn-lg" href="#" role="button">Beli Sekarang</a>
+          </div>
+        </div>
+      </div>
+      <hr class="my-4" />
+      <div class="card-deck">
+        <div class="card">
+          <img src="./assets/picture/produkd.jpg" class="card-img-top" alt="Produk d" />
+          <div class="card-body">
+            <h5 class="card-title">Kaos D</h5>
+            <p class="card-text">Rp 85.000</p>
+            <p class="card-text"><small class="text-muted">Stok 100</small></p>
+            <a class="btn btn-primary btn-lg" href="#" role="button">Beli Sekarang</a>
+          </div>
+        </div>
+        <div class="card">
+          <img src="./assets/picture/produke.jpg" class="card-img-top" alt="Produk e" />
+          <div class="card-body">
+            <h5 class="card-title">Kaos E</h5>
+            <p class="card-text">Rp 120.000</p>
+            <p class="card-text"><small class="text-muted">Stok 50</small></p>
+            <a class="btn btn-primary btn-lg" href="#" role="button">Beli Sekarang</a>
+          </div>
+        </div>
+        <div class="card">
+          <img src="./assets/picture/produkf.jpg" class="card-img-top" alt="Produk f" />
+          <div class="card-body">
+            <h5 class="card-title">Kaos F</h5>
+            <p class="card-text">Rp 125.000</p>
+            <p class="card-text"><small class="text-muted">Stok 40</small></p>
+            <a class="btn btn-primary btn-lg" href="#" role="button">Beli Sekarang</a>
+          </div>
+        </div>
+      </div>
+      <hr class="my-4" />
+      <div class="card-deck">
+        <div class="card">
+          <img src="./assets/picture/produkg.jpg" class="card-img-top" alt="Produk g" />
+          <div class="card-body">
+            <h5 class="card-title">Kaos G</h5>
+            <p class="card-text">Rp 350.000</p>
+            <p class="card-text"><small class="text-muted">Stok 150</small></p>
+            <a class="btn btn-primary btn-lg" href="#" role="button">Beli Sekarang</a>
+          </div>
+        </div>
+        <div class="card">
+          <img src="./assets/picture/produkh.jpg" class="card-img-top" alt="Produk h" />
+          <div class="card-body">
+            <h5 class="card-title">Kaos H</h5>
+            <p class="card-text">Rp 65.000</p>
+            <p class="card-text"><small class="text-muted">Stok 196</small></p>
+            <a class="btn btn-primary btn-lg" href="#" role="button">Beli Sekarang</a>
+          </div>
+        </div>
+        <div class="card">
+          <img src="./assets/picture/produki.jpg" class="card-img-top" alt="Produk i" />
+          <div class="card-body">
+            <h5 class="card-title">Kaos I</h5>
+            <p class="card-text">Rp 95.000</p>
+            <p class="card-text"><small class="text-muted">Stok 190</small></p>
+            <a class="btn btn-primary btn-lg" href="#" role="button">Beli Sekarang</a>
+          </div>
+        </div>
+      </div>
+    </div>
+    <footer>
+      <p class="copyright">&copy; 2023 Contoh Website Jual Beli Kaos</p>
+    </footer>
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-Fy6S3B9q64WdZWQUiU+q4/2Lc9npb8tCaSX9FK7E8HnRr0Jz8D6OP9dO5Vg3Q9ct" crossorigin="anonymous"></script>
+  </body>
+</html>
